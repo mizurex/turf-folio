eec228799a9c3d50fac81a1ca81f258f873409bf
mizurex <viswasdhal@gmail.com>
2025-09-07 16:18:40 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,265 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import { WordRotate } from "@/components/WordsFlip";
+import { ExternalLink, ListStartIcon,  } from "lucide-react";
+
+const cn = (...classes: string[]) => classes.join(" ");
+
+const projects = [
+  {
+    image: "/widget.png",
+    live: "https://yourportfolio.com",
+    github: "https://github.com/org/portfolio",
+  },
+  {
+    image: "/minimal.png",
+    live: "https://billingdemo.com",
+    github: "https://github.com/org/repo",
+  },
+  {
+    image: "/pixel.png",
+    live: "https://yourchatbotdemo.com",
+    github: "https://github.com/org/chatbot-widget",
+  },
+  {
+    image: "/printin.png",
+    live: "https://mystore.com",
+    github: "https://github.com/org/store",
+  },
+  {
+    image: "/baka.png",
+    live: "https://blogsite.com",
+    github: "https://github.com/org/blog",
+  },
+  {
+    image: "/projects/weather.png",
+    live: "https://weatherapp.com",
+    github: "https://github.com/org/weather",
+  },
+];
 
 export default function Home() {
+  const [showMore, setShowMore] = useState(4);
+  
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="flex flex-col items-center justify-center bg-white text-gray-800 py-2">
+      <div className="max-w-3xl w-full bg-white pt-2 px-1 border-x  border-gray-300">
+        <div className=" w-[700px] h-[100px] flex justify-center rounded-lg border-b border-gray-300 mx-auto">
+          <div >
+        <h4 className="text-4xl text-center font-mono text-gray-900 mt-8"><WordRotate words={["HI!!", "HELLO!!", "WELCOME!!", "GOOD DAY!!", "GOOD MORNING!!", "GOOD EVENING!!", "GOOD NIGHT!!"]} /></h4>
+        </div>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+               
+        
+        <div className="font-mono">
+        <p className="text-gray-500 font-mono">I'm a novice programmer and this is my portfolio. I love to learn new things and make things.</p>
+        <p className="text-gray-500 font-mono">Having made so many things and learned so many things, I decided to make a portfolio to show off my work and my skills.</p>
+        <p className="text-gray-500 font-mono">Have a look at my work and feel free to suggest improvements for my projects.</p>
+        </div>
+        <div className="h-[30px] overflow-hidden border border-gray-300 rounded-lg">
+           <Separator />
+        </div>
+        <div className="flex grid grid-cols-3 overflow-hidden">
+          <div className="col-span-2 flex justify-center items-center border border-gray-200 rounded-lg overflow-hidden">
+            <h2 className="text-4xl text-center font-mono text-gray-900 mt-8">Check Out My Projects</h2>
+          </div>
+          <div className="col-span-1  border border-gray-200 rounded-lg overflow-hidden">
+            <Separator />
+          </div>    
+        </div>
+        
+        <div className="h-[30px] overflow-hidden border border-gray-300 rounded-lg">
+           <Separator />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 border border-gray-300 rounded-lg py-5 px-5 overflow-hidden">
+          {projects.slice(0, showMore).map((project, index) => (
+         <div
+         key={index}
+         className="group relative h-[220px] rounded-lg border border-gray-200 overflow-hidden"
+       >
+         {/* Project thumbnail */}
+         <Image
+           src={project.image}
+           alt="Project thumbnail"
+           fill
+           className="object-cover group-hover:scale-105 transition-transform"
+         />
+
+         {/* Action icons */}
+         <div className="absolute bottom-2 right-2 flex gap-2">
+           <a
+             href={project.live}
+             target="_blank"
+             rel="noopener noreferrer"
+             className="p-2 bg-white rounded-full shadow hover:bg-gray-100 transition"
+           >
+             <ExternalLink className="w-4 h-4 text-gray-700" />
+           </a>
+           <a
+             href={project.github}
+             target="_blank"
+             rel="noopener noreferrer"
+             className="p-2 bg-white rounded-full shadow hover:bg-gray-100 transition"
+           >
+             <ListStartIcon className="w-4 h-4 text-gray-700" />
+           </a>
+         </div>
+       </div>
+          ))}
+          
+          {showMore < projects.length && (
+            <div 
+              className="text-center text-gray-600 hover:text-black cursor-pointer font-medium py-6 md:col-span-2 " 
+              onClick={() => setShowMore(showMore + 4)}
+            >
+              Show More
+            </div>
+          )}
+        </div>
+        
+        <div className="h-[30px] overflow-hidden border border-gray-300 rounded-lg">
+           <Separator />
+        </div>
+       
+        <div className="flex grid grid-cols-3 overflow-hidden ">
+          <div className="col-span-2 flex justify-center items-center border border-gray-200 rounded-lg overflow-hidden">
+            <h2 className="text-3xl font-bold text-gray-900">Tools & Tech I Use</h2>
+          </div>
+          <div className="col-span-1  border border-gray-200 rounded-lg overflow-hidden">
+            <Separator />
+          </div>
+
+          <div className=" grid grid-cols-3 justify-center items-center border border-gray-200 rounded-lg overflow-hidden ">
+          <div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="ts.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div><div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="react.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div><div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="nextjs2-light.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div><div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="tailwindcss.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div><div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="js.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div>
+          <div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="mysql.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div>
+          </div>
+          <div className="col-span-1  border border-gray-200 rounded-lg overflow-hidden">
+            <Separator />
+          </div>
+          <div className=" grid grid-cols-3 justify-center items-center border border-gray-200 rounded-lg overflow-hidden">
+          <div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="nodejs.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div><div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="mongodb.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div><div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="motion.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div><div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="git.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div>
+          <div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="docker.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div>
+          <div className="flex justify-center">
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+              <img src="ts.svg" alt="TypeScript" className="w-12 h-12" />
+            </a>
+          </div>
+          
+          </div>
+
+
+        </div>
+     
+        <div className="flex grid grid-cols-3 overflow-hidden">
+        <div className="col-span-1  border border-gray-200 rounded-lg overflow-hidden">
+            <Separator />
+          </div>
+          <div className="col-span-2 flex justify-center items-center border border-gray-200 rounded-lg overflow-hidden">
+            <h2 className="text-3xl font-bold text-gray-900">My Contributions</h2>
+          </div>
+         
+          <div className="col-span-1  border border-gray-200 rounded-lg overflow-hidden"> 
+            <span className="text-gray-500">dodopayments 2 merged PRs</span>
+          </div>
+          <div className="col-span-1  border border-gray-200 rounded-lg overflow-hidden"> 
+            <span className="text-gray-500">dodopayments 2 merged PRs</span>
+          </div>
+          <div className="col-span-1  border border-gray-200 rounded-lg overflow-hidden"> 
+            <span className="text-gray-500">dodopayments 2 merged PRs</span>
+          </div>
+        </div>
+
+
+
+
+        <div className="h-[30px] overflow-hidden border border-gray-300 rounded-lg">
+           <Separator />
+        </div>
+        
+        <div className="flex items-center justify-end">
+          
+          <div>
+          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+            <img src="/github.webp" alt="GitHub" className="w-12 h-12" />
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div></div>
+        <div>
+          <a href="https://linkedin.com/yourusername" target="_blank" rel="noopener noreferrer">
+            <img src="linkedin.webp" alt="LinkedIn" className="w-12 h-12" />
+          </a>
+        </div>
+        <div></div>
+        <div>
+          <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+            <img src="x.webp" alt="Twitter" className="w-12 h-12" />
+          </a>
+        </div>
+        </div>
+      </div>
+    
+
+      <div className="text-center text-gray-600">
+      <p>Inspired by <a href="https://leo-neves.com/" target="_blank" rel="noopener noreferrer">Leo Neves</a></p>
+      </div>
+    </main>
+  );
+}
+
+function Separator() {
+  return (
+    <div
+      className="relative flex h-[100px] w-full 
+        before:absolute before:inset-0
+        before:bg-[repeating-linear-gradient(315deg,#e5e7eb_0,#d1d5db_1px,transparent_0,transparent_50%)]
+        before:bg-[length:10px_10px] border-t border-gray-200"
+    />
   );
 }
